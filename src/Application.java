3dd3d3b71854186147795ff7b378e776a0f70885
Clément Bosc <clement.bosc@outlook.com>
2017-02-11 22:08:50 +0100

    Demande à l'utilisateur à quel emplacement il vaut stocker la base de donnée ou dans quelle table

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by clementbosc on 11/02/2017.
@@ -6,33 +7,65 @@ import java.io.FileNotFoundException;
 public class Application {
 
     //Chemin vers la base de donnée sqlite3
-    public static final String DATABASE_PATH = "db/database.sqlite3";
+    public String databasePath;
 
     //Chemin vers le fichier csv à importer
-    public static final String CSV_FILE_PATH = "docs/file.csv";
+    public String csvPath;
 
     //Table à créer et dans laquelle insérer les données
-    public static final String TABLE_NAME = "data";
+    public String tableName;
 
 
     /**
      * Application constructor
      */
     public Application() throws FileNotFoundException {
+
+        //Initialisation du programme
+        initialisation();
+
         //Instanciation de SQLiteDB
-        SQLiteDB database = new SQLiteDB(DATABASE_PATH);
+        SQLiteDB database = new SQLiteDB(databasePath);
 
         //création de la table si elle n'existe pas
-        database.createTable(TABLE_NAME);
+        database.createTable(tableName);
 
         //Instanciation du CSVReader
-        CSVReader reader = new CSVReader(CSV_FILE_PATH, ",");
+        CSVReader reader = new CSVReader(csvPath, ",");
 
         //On génère la requête d'insertion
-        String query = reader.getSQLInsertStatement(TABLE_NAME);
+        String query = reader.getSQLInsertStatement(tableName);
 
         //On exécute la requête
         database.execQuery(query);
+
+        System.out.println("\n\nTraitement terminé : les données ont été inséré dans la table "+tableName+" du fichier SQLite "+databasePath);
+    }
+
+
+    /**
+     * Fonction d'initialisation du programme
+     */
+    public void initialisation(){
+        System.out.println("*************************************");
+        System.out.println("****   CSV -> SQLite Converter   ****");
+        System.out.println("*************************************");
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n\nVeuillez entrer un fichier .sqlite3 dans lequel les données seront insérées " +
+                "(si le fichier n'existe pas il sera créé) :");
+
+        this.databasePath = sc.nextLine();
+
+        System.out.println("\n\nVeuillez entrer une table dans laquelle les données seront insérées " +
+                "(si la table n'existe pas elle sera créé, sinon les données seront ajoutées à la suite de celles existantes) :");
+
+        this.tableName = sc.nextLine();
+
+        System.out.println("\n\nVeuillez entrer un fichier CSV dans lequel les données sont actuellement stockées : ");
+
+        this.csvPath = sc.nextLine();
     }
 
     public static void main(String[] args) {
